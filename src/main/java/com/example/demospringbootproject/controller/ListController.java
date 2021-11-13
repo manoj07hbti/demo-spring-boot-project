46dc5e854754805c401d6e179634f448ed2aecb3
60manojkumar@gmail.com <Motivated@2019>
2021-11-13 08:49:34 +0530

    List Controller

---
+++ src/main/java/com/example/demospringbootproject/controller/ListController.java
@@ -0,0 +1,48 @@
+package com.example.demospringbootproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ListController {
+
+    List<String> stringList= new ArrayList<>();
+
+    // CRUD - C - Create/insert/add , R- read , U-Update , D - delete
+//C - Create/insert/add
+    @RequestMapping("/add")
+    public String addToList(){
+
+        stringList.add("Java");
+
+        return "Object is successfully added to the list";
+    }
+
+    //Read or get
+
+    @RequestMapping("/get")
+    public  List<String> getStringList(){
+
+        return stringList;
+    }
+
+    //Update
+
+    @RequestMapping("/update")
+    public String updateList(){
+
+
+        stringList.add(0,"Spring");
+        return "Object is  updated successfully";
+    }
+
+    //delete / remove
+    @RequestMapping("/remove")
+    public String deleteObject(){
+        stringList.remove(0);
+        return "Object is  deleted successfully";
+    }
+}
